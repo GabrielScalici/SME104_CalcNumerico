fd61d30c9e06849a33b19e1324055edc70304f85
GabrielScalici <gabrielhc.scalici@gmail.com>
2018-04-27 18:44:48 -0300

    nao funcionando

--- main.c
+++ main.c
@@ -1,19 +1,18 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include<math.h>
 
-
 int main(void){
-
-    float ae, max, t, s, e;
-    int i, j, r, mxit, n;
+    float tol, error;
+    int i, j, k, itmax, n, s1, s2;
 
     //Pegando a dimensao da matriz
     scanf("%d", &n);
 
     //Criando a matriz
-    float** x = (float**) malloc (n*sizeof(float*));
+    float** A = (float**) malloc (n*sizeof(float*));
     for(i = 0; i < n; i++){
-      x[i] = (float*) malloc (n*sizeof(float));
+      A[i] = (float*) malloc (n*sizeof(float));
     }
 
     //Atribuindo os valores na matriz de acordo com a especificação
@@ -21,17 +20,17 @@ int main(void){
       for(j = 0; j < n; j++){
         //Posicoes especificas
         if(i == j){
-          x[i][j] = 4;
-        }else if(j == i+1 && i <= n-1){
-          x[i][j] = -1;
-        }else if(i == j+1 && j <= n-1){
-          x[i][j] = -1;
-        }else if(j == i+3 && i <= n-3){
-          x[i][j] = -1;
-        }else if(i == j+3 && i <= n-3){
-          x[i][j] = -1;
+          A[i][j] = 4;
+        }else if(j == i+1 && i < n-1){
+          A[i][j] = -1;
+        }else if(i == j+1 && j < n-1){
+          A[i][j] = -1;
+        }else if(j == i+3 && i < n-3){
+          A[i][j] = -1;
+        }else if(i == j+3 && i < n-3){
+          A[i][j] = -1;
         }else{
-          x[i][j] = 0; //Colocar 0 nas restantes
+          A[i][j] = 0; //Colocar 0 nas restantes
         }
       }
     }
@@ -42,56 +41,58 @@ int main(void){
     for(i = 0; i < n; j++){
       int aux = 0;
       for(j = 0; j < n; j++){
-        aux += x[i][j];
+        aux += A[i][j];
       }
       b[i] = aux;
     }
 
     //Criando vetor x
-    float* b = (float*) malloc(n*sizeof(float));
+    float* x = (float*) malloc(n*sizeof(float));
     //Zerando todas as posicoes do vetor
     for(i = 0; i < n; i++){
-      b[i] = 0;
+      x[i] = 0;
     }
 
+    //Criando vetor y (x anterior)
+    float* y = (float*) malloc(n*sizeof(float));
+    //Zerando todas as posicoes do vetor
+    for(i = 0; i < n; i++){
+      y[i] = 0;
+    }
 
     //Pegando os valores do erro e numero maximo de iteracoes
     printf("Insira o erro(e) desejado e o número máximo de iterações: ");
-    scanf("%f %d",&ae,&mxit);
+    scanf("%f %d", &tol, &itmax);
 
-
-    printf("Iteration\tx[1]\tx[2]\n");
-    for(r = 1; r <= mxit; r++){
-        max=0;
+    //Laco principal
+    for(k = 0; k < itmax; k++){
+        //Guardando x antigo
+        for(i = 0; i < n; i++){
+          y[i] = x[j];
+        }
 
         for(i = 0; i < n; i++){
-            s2=0;
+            s1 = 0;
+            s2 = 0;
 
-            for(j = 0; j < n ; j++){
-              if(j != i){
-                s2 += x[i][j] * x[j];
-              }
+            for(j = i; j < n - 1; j++){
+                s2 += A[i][j] * x[j];
             }
 
-            t = ( x[i][n] - s2) / x[i][i];
-            // b - s1 - s2 / diag
+            for(j = 0; j < i; j++){
+                s1 += A[i][j] * x[j]; //precisa usar k+1
+            }
 
-            e = fabs(b[i] - t);
-            b[i] = t;
+            x[i] = (b[i] - s1 - s2) / A[i][i];
+
+            //precisa arrumar
+            //error = fabs(100000000);
+            error = 100000000000;
+
+            if(error < tol)
+              k = itmax; //sai do laco
         }
+      }
 
-
-        printf(" %5d\t",r);
-        for(i=0;i<n;i++)
-        printf(" %9.4f\t",a[i]);
-        printf("\n");
-
-        if(max<ae){
-            printf(" Converses in %3d iteration\n", r);
-            for(i=0;i<n;i++)
-              printf("a[%3d]=%7.4f\n", i+1,a[i]);
-            return 0;
-        }
-
-        }
+        return 0;
     }
